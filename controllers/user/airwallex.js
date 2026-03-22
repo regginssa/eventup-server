cb4c74e04c199b81dff96ff560bef7a769c32adf
regginssa <randyarabis428@gmail.com>
2026-03-23 00:52:06 +0900

    Remove customer from airwallex

--- controllers/user/airwallex.js
+++ controllers/user/airwallex.js
@@ -194,47 +194,6 @@ const webhook = async (req, res) => {
   }
 };
 
-const customer = {
-  create: async (req, res) => {
-    try {
-      const user = await User.findById(req.user.id);
-
-      if (!user) {
-        return res.json({ ok: false, message: "User not found" });
-      }
-
-      const { email, firstName, lastName } = user;
-
-      const customerId = await service.customer.create({
-        email,
-        firstName,
-        lastName,
-        userId: user._id.toString(),
-      });
-
-      if (!customerId) {
-        return res.json({ ok: false, data: null });
-      }
-
-      const pit = await service.paymentIntent.create({
-        amount: 0,
-        currency: "USD",
-        customerId,
-      });
-
-      if (!pit) {
-        return res.json({ ok: false, data: null });
-      }
-
-      user.airwallexCustomerId = customerId;
-      await user.save();
-      res.json({ ok: true, data: pit });
-    } catch (e) {
-      res.status(500).json({ ok: false, messaeg: "internal server error" });
-    }
-  },
-};
-
 const paymentIntent = {
   create: async (req, res) => {
     try {
@@ -250,4 +209,4 @@ const paymentIntent = {
   },
 };
 
-module.exports = { webhook, customer, paymentIntent };
+module.exports = { webhook, paymentIntent };
--- routes/user/airwallex.js
+++ routes/user/airwallex.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
-const { customer, paymentIntent } = require("../../controllers/user/airwallex");
+const { paymentIntent } = require("../../controllers/user/airwallex");
 
-router.post("/customer", customer.create);
 router.post("/payment-intent", paymentIntent.create);
 
 module.exports = router;
